adadfcb3983d6375d8679a0b2db9d172076fa07c
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-02-26 00:12:20 -0600

    Migrated random number generation code to its own RandomNumberJob class. Keeping the "nextRandom" route for now to allow for manual triggering.

---
+++ Sources/App/Jobs/RandomNumberJob.swift
@@ -0,0 +1,72 @@
+//
+//  RandomNumberJob.swift
+//  App
+//
+//  Created by Tyler Milner on 2/25/18.
+//
+
+import Foundation
+import Dispatch
+
+class RandomNumberJob {
+    
+    // MARK: - Properties
+    
+    private static let randomNumberInterval: TimeInterval = 60 // Next update in 60 seconds
+    private static let timer = DispatchSource.makeTimerSource()
+    
+    // MARK: - Public
+    
+    static func scheduleRandomNumberJob(using droplet: Droplet) {
+        timer.setEventHandler {
+            publishNewRandomNumber(using: droplet)
+        }
+        
+        // Fire the timer immediately and then every X seconds, +/- 1 second leeway
+        timer.schedule(deadline: .now(), repeating: randomNumberInterval, leeway: .seconds(1))
+        timer.resume()
+    }
+    
+    @discardableResult
+    static func publishNewRandomNumber(using droplet: Droplet) -> Response {
+        do {
+            let googleOAuth = GoogleOAuth(jwtSignerName: "googleOAuth", serviceAccountEmail: "firebase-adminsdk-notoj@vaporfirebasedemo.iam.gserviceaccount.com", droplet: droplet)
+            
+            let authResponse = try googleOAuth.authenticateWithGoogle(using: droplet.client)
+            
+            // Google OAuth - Handle the response from Google
+            switch authResponse.status {
+            case .ok:
+                guard let accessToken: String = try authResponse.json?.get("access_token") else { return Response(status: .internalServerError, body: "Google auth response did not include an access token") }
+                
+                // Generate a new random number and date for the next one to be generated
+                let randomNumber = Int(arc4random_uniform(100) + 1) // Generates a random number between [1-100]
+                let nextUpdate = Date().addingTimeInterval(randomNumberInterval)
+                
+                let firestore = Firestore(accessToken: accessToken)
+                
+                // Firebase - Create a Firestore document object to represent the random number
+                let randomNumberDocument = NumberDocument(value: randomNumber,
+                                                          nextUpdate: nextUpdate,
+                                                          projectId: "vaporfirebasedemo",
+                                                          databaseId: "(default)",
+                                                          documentPath: "randomNumbers/theRandomNumber")
+                
+                // Firebase - PATCH onto the resource name to insert or update the document
+                let firestoreResponse = try firestore.updateDocument(randomNumberDocument, using: droplet.client)
+                
+                switch firestoreResponse.status {
+                case .ok:
+                    return firestoreResponse
+                default:
+                    return Response(status: .internalServerError, body: "Error saving document to Firestore: \(firestoreResponse)")
+                }
+            default:
+                return Response(status: .internalServerError, body: "Unexpected Google auth response: '\(authResponse)")
+            }
+        } catch {
+            debugPrint("\(error)")
+            return Response(status: .internalServerError, body: "\(error)")
+        }
+    }
+}
--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -6,45 +6,7 @@ extension Droplet {
     func setupRoutes() throws {
         
         post("nextRandom") { request in
-            do {
-                let googleOAuth = GoogleOAuth(jwtSignerName: "googleOAuth", serviceAccountEmail: "firebase-adminsdk-notoj@vaporfirebasedemo.iam.gserviceaccount.com", droplet: self)
-                
-                let authResponse = try googleOAuth.authenticateWithGoogle(using: self.client)
-                
-                // Google OAuth - Handle the response from Google
-                switch authResponse.status {
-                case .ok:
-                    guard let accessToken: String = try authResponse.json?.get("access_token") else { return Response(status: .internalServerError, body: "Google auth response did not include an access token") }
-                    
-                    // Generate a new random number and date for the next one to be generated
-                    let randomNumber = Int(arc4random_uniform(100) + 1) // Generates a random number between [1-100]
-                    let nextUpdate = Date().addingTimeInterval(60) // Next update in 60 seconds
-                    
-                    let firestore = Firestore(accessToken: accessToken)
-                    
-                    // Firebase - Create a Firestore document object to represent the random number
-                    let randomNumberDocument = NumberDocument(value: randomNumber,
-                                                              nextUpdate: nextUpdate,
-                                                              projectId: "vaporfirebasedemo",
-                                                              databaseId: "(default)",
-                                                              documentPath: "randomNumbers/theRandomNumber")
-                    
-                    // Firebase - PATCH onto the resource name to insert or update the document
-                    let firestoreResponse = try firestore.updateDocument(randomNumberDocument, using: self.client)
-                    
-                    switch firestoreResponse.status {
-                    case .ok:
-                        return firestoreResponse
-                    default:
-                        return Response(status: .internalServerError, body: "Error saving document to Firestore: \(firestoreResponse)")
-                    }
-                default:
-                    return Response(status: .internalServerError, body: "Unexpected Google auth response: '\(authResponse)")
-                }
-            } catch {
-                debugPrint("\(error)")
-                return Response(status: .internalServerError, body: "\(error)")
-            }
+            return RandomNumberJob.publishNewRandomNumber(using: self)
         }
     }
 }
