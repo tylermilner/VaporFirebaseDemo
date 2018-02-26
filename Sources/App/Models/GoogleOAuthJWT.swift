0ce8c64ad73e7f37571bb23bb9b616474e3ac164
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-02-25 19:28:34 -0600

    Minor cleanup of the Google OAuth JWT generation - created GoogleOAuthJWT struct to handle basic operation of generating the header and claims JSON.

---
+++ Sources/App/Models/GoogleOAuthJWT.swift
@@ -0,0 +1,46 @@
+//
+//  GoogleOAuthJWT.swift
+//  App
+//
+//  Created by Tyler Milner on 2/25/18.
+//
+
+import Vapor
+import Foundation
+
+/// Represents the JWT used to authenticate with Google using their server-to-server OAuth 2.0 process.
+struct GoogleOAuthJWT {
+    
+    // MARK: - Properties
+    
+    private let serviceAccountEmail: String
+    
+    // MARK: - Init
+    
+    init(serviceAccountEmail: String) {
+        self.serviceAccountEmail = serviceAccountEmail
+    }
+    
+    // MARK: - Public
+    
+    func generateHeaders() throws -> JSON {
+        var headersJSON = JSON()
+        try headersJSON.set("alg", "RS256")
+        try headersJSON.set("typ", "JWT")
+        return headersJSON
+    }
+    
+    func generateClaims() throws -> JSON {
+        let issuedTime = Date()
+        let timeToLive: TimeInterval = 60 * 30 // 30 minutes (max is 60)
+        let expirationTime = issuedTime.addingTimeInterval(timeToLive)
+        
+        var claimsJSON = JSON()
+        try claimsJSON.set("iss", serviceAccountEmail) // the value of "client_email" in the service account private key JSON
+        try claimsJSON.set("scope", "https://www.googleapis.com/auth/datastore")
+        try claimsJSON.set("aud", "https://www.googleapis.com/oauth2/v4/token")
+        try claimsJSON.set("exp", Int(expirationTime.timeIntervalSince1970))
+        try claimsJSON.set("iat", Int(issuedTime.timeIntervalSince1970))
+        return claimsJSON
+    }
+}
--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -7,24 +7,9 @@ extension Droplet {
         
         post("nextRandom") { request in
             do {
-                // Google OAuth - Create the JWT headers
-                var headersJSON = JSON()
-                try headersJSON.set("alg", "RS256")
-                try headersJSON.set("typ", "JWT")
-                debugPrint("Headers: \(headersJSON)")
-                
-                // Google OAuth - Create the JWT claims
-                let issuedTime = Date()
-                let timeToLive: TimeInterval = 60 * 30 // 30 minutes, max is 60
-                let expirationTime = issuedTime.addingTimeInterval(timeToLive)
-                
-                var claimsJSON = JSON()
-                try claimsJSON.set("iss", "firebase-adminsdk-notoj@vaporfirebasedemo.iam.gserviceaccount.com") // the value of "client_email" in the service account JSON
-                try claimsJSON.set("scope", "https://www.googleapis.com/auth/datastore")
-                try claimsJSON.set("aud", "https://www.googleapis.com/oauth2/v4/token")
-                try claimsJSON.set("exp", Int(expirationTime.timeIntervalSince1970))
-                try claimsJSON.set("iat", Int(issuedTime.timeIntervalSince1970))
-                debugPrint("Claims: \(claimsJSON)")
+                let googleOAuthJWT = GoogleOAuthJWT(serviceAccountEmail: "firebase-adminsdk-notoj@vaporfirebasedemo.iam.gserviceaccount.com")
+                let headersJSON = try googleOAuthJWT.generateHeaders()
+                let claimsJSON = try googleOAuthJWT.generateClaims()
                 
                 // Google OAuth - Create the JWT signature
                 guard let oAuthSigner = self.signers?["googleOAuth"] else { return Response(status: .internalServerError, body: "Unable to locate signer") }
