50b975c2969c3079fda6bef8f8c52c5570b1a30d
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-03-13 17:56:59 -0500

    Generating the random number and sending it to Firebase.

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -40,9 +40,39 @@ extension Droplet {
                 case .ok:
                     guard let accessToken: String = try authResponse.json?.get("access_token") else { return Response(status: .internalServerError, body: "Google auth response did not include an access token") }
                     
-                    // ...
+                    // Generate a new random number
+                    let randomNumber = arc4random_uniform(100) + 1 // Generates a random number between [1-100]
                     
-                    return Response(status: .methodNotAllowed)
+                    // Firebase - Build the Cloud Firestore resource name
+                    let projectId = "vaporfirebasedemo"
+                    let databaseId = "(default)"
+                    let documentPath = "randomNumbers/theRandomNumber"
+                    let resourceName = "projects/\(projectId)/databases/\(databaseId)/documents/\(documentPath)"
+                    
+                    // Firebase - Create a Firestore document object to represent the random number
+                    var documentJSON = JSON()
+                    try documentJSON.set("name", "\(resourceName)") // The Firestore resource name of the document, for example projects/{projectId}/databases/{databaseId}/documents/{document_path}
+                    
+                    var numberValueJSON = JSON()
+                    try numberValueJSON.set("integerValue", randomNumber)
+                    
+                    var numberJSON = JSON()
+                    try numberJSON.set("number", numberValueJSON)
+                    
+                    try documentJSON.set("fields", numberJSON)
+                    
+                    let firestoreBaseURL = "https://firestore.googleapis.com/v1beta1"
+                    let firestoreDocumentURL = "\(firestoreBaseURL)/\(resourceName)"
+                    
+                    // Firebase - PATCH onto the resource name to insert or update the document
+                    let firestoreResponse = try self.client.patch(firestoreDocumentURL, query: [:], [.authorization: "Bearer \(accessToken)"], documentJSON, through: [])
+                    
+                    switch firestoreResponse.status {
+                    case .ok:
+                        return firestoreResponse
+                    default:
+                        return Response(status: .internalServerError, body: "Error saving document to Firestore: \(firestoreResponse)")
+                    }
                 default:
                     return Response(status: .internalServerError, body: "Unexpected Google auth response: '\(authResponse.status)")
                 }
